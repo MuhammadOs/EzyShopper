4a79e5457dbd9024d5f0485374f5183b86581f71
Ahmed Samir <ahmedsamiirr20@gmail.com>
2024-11-20 14:27:46 +0200

    commit to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,41 +11,8 @@ import { CommonModule, NgFor } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  featuredProducts: Product[] = [];
-  allProducts: Product[] = [];
-  selectedProduct: Product | null = null;
-  categories: string[] = [];
-  productsByCategory: Product[] = [];
-  loadingFeatured = true;
-  loadingAll = true;
-  loadingCategories = true;
-  loadingProductsByCategory = true;
+export class HomeComponent {
 
-  private productService = inject(ProductService);
-
-  ngOnInit(): void {
-    // Get all products
-    this.productService.getAllProducts().subscribe(products => {
-      this.allProducts = products;
-      this.loadingAll = false;
-    });
-
-    // Get product by ID (Example: Fetching product with ID '1')
-    this.productService.getProductById('1').subscribe(product => {
-      this.selectedProduct = product;
-    });
-
-    // Get all categories
-    this.productService.getAllCategories().subscribe(categories => {
-      this.categories = categories;
-      this.loadingCategories = false;
-    });
-
-    // Get products by category name (Example: Fetching products in 'electronics' category)
-    this.productService.getProductsByCategoryName('electronics').subscribe(products => {
-      this.productsByCategory = products;
-      this.loadingProductsByCategory = false;
-    });
-  }
+  
+  
 }
