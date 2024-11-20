ff7287afa4a6f6de3d5091e4e0ff00e39f3fc5d4
Ahmed Samir <ahmedsamiirr20@gmail.com>
2024-11-20 16:21:00 +0200

    add footer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 // app.component.ts
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']})
 export class AppComponent {
